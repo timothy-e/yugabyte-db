a5713982c4e2eee1ab06b2b186feb1b69d897846
Tim Elgersma <timothy.elgersma@gmail.com>
2022-12-16 08:54:51 -0500

    check lockout in session initialization

--- src/postgres/src/backend/utils/init/miscinit.c
+++ src/postgres/src/backend/utils/init/miscinit.c
@@ -13,7 +13,6 @@
  *-------------------------------------------------------------------------
  */
 
-#include <pg_yb_utils.h>
 #include "postgres.h"
 
 #include <sys/param.h>
@@ -54,6 +53,10 @@
 #include "utils/syscache.h"
 #include "utils/varlena.h"
 
+/* YB includes */
+#include <pg_yb_utils.h>
+#include "catalog/pg_yb_role_profile.h"
+#include "commands/yb_profile.h"
 
 #define DIRECTORY_LOCK_FILE		"postmaster.pid"
 
@@ -594,7 +597,9 @@ void
 InitializeSessionUserId(const char *rolename, Oid roleid)
 {
 	HeapTuple	roleTup;
+	HeapTuple	rolPrfTup;
 	Form_pg_authid rform;
+	Form_pg_yb_role_profile rpform;
 	char	   *rname;
 
 	/*
@@ -670,6 +675,21 @@ InitializeSessionUserId(const char *rolename, Oid roleid)
 					(errcode(ERRCODE_TOO_MANY_CONNECTIONS),
 					 errmsg("too many connections for role \"%s\"",
 							rname)));
+
+		if (*YBCGetGFlags()->ysql_enable_profile && YbLoginProfileCatalogsExist)
+		{
+			rolPrfTup = get_role_profile_tuple_by_role_oid(MyProc->roleId);
+			if (HeapTupleIsValid(rolPrfTup))
+			{
+				rpform = (Form_pg_yb_role_profile) GETSTRUCT(rolPrfTup);
+
+				if (rpform->rolprfstatus != ROLPRFSTATUS_OPEN)
+					ereport(FATAL,
+							(errcode(ERRCODE_INVALID_AUTHORIZATION_SPECIFICATION),
+							 errmsg("role \"%s\" is locked. Contact your database administrator.",
+									rname)));
+			}
+		}
 	}
 
 	/* Record username and superuser status as GUC settings too */
